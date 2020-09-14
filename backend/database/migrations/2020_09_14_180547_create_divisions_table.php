d9dd6cec855a109e51205f1c7162bbd2318b6e37
Héctor Chumpitaz <hectorchumpitazwatanave@outlook.com>
2020-09-14 13:57:52 -0500

    Primer Commit: Proyecto Laravel y React

---
+++ backend/database/migrations/2020_09_14_180547_create_divisions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDivisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('divisions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45)->unique();
+            $table->unsignedBigInteger('upper_division')->nullable();
+            $table->foreign('upper_division')->references('id')->on('divisions');
+            $table->integer('collaborators');
+            $table->string('ambassor', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('divisions');
+    }
+}
